63b3f000cab1653b2ec72c72f86ed738e29b01dc
maruan <maruanbakriottoni@gmail.com>
2024-04-15 13:28:29 -0300

    added model selection with kl div

--- src/model_selection.py
+++ src/model_selection.py
@@ -63,3 +63,72 @@ class RandomGraphModelSelector:
         self.best_model = min(self.model_scores, key=self.model_scores.get)
         return self.best_model, self.model_scores
 
+
+
+class ModelSelectorSpectrum:
+    def __init__(self, real_graph, logit_graph):
+        self.real_graph = real_graph
+        self.logit_graph = logit_graph
+        self.models = {'ER': self.erdos_renyi, 'WS': self.watts_strogatz, 'BA': self.barabasi_albert, 'LG': self.logit_graph}
+        self.best_model = None
+        self.model_scores = {}
+
+    def erdos_renyi(self, n, p):
+        return nx.erdos_renyi_graph(n, p)
+
+    def watts_strogatz(self, n, k, p):
+        return nx.watts_strogatz_graph(n, k, p)
+
+    def barabasi_albert(self, n, m):
+        return nx.barabasi_albert_graph(n, m)
+
+    def graph_spectrum(self, graph):
+        laplacian = nx.laplacian_matrix(graph).astype(float)
+        eigenvalues = np.linalg.eigvalsh(laplacian.toarray())
+        return eigenvalues
+
+    def kl_divergence(self, real_spectrum, model_spectrum):
+        hist_real, bins = np.histogram(real_spectrum, bins=100, density=True)
+        hist_model, _ = np.histogram(model_spectrum, bins=bins, density=True)
+        hist_real += np.finfo(float).eps
+        hist_model += np.finfo(float).eps
+        kl_div = np.sum(hist_real * np.log(hist_real / hist_model))
+        return kl_div
+
+    def model_penalty(self, model_name):
+        penalties = {'ER': 2, 'WS': 3, 'BA': 2, 'LG': 3}
+        return penalties.get(model_name, 0)
+
+    def evaluate_model(self, model_graph, model_name):
+        real_spectrum = self.graph_spectrum(self.real_graph)
+        model_spectrum = self.graph_spectrum(model_graph)
+        kl_div = self.kl_divergence(real_spectrum, model_spectrum)
+        penalty = self.model_penalty(model_name)
+
+        score = kl_div - penalty
+        return score
+
+    def fit(self):
+        n = len(self.real_graph.nodes())
+        p = np.mean([d for n, d in self.real_graph.degree()]) / (n - 1)
+        k = int(np.mean(list(dict(self.real_graph.degree()).values())))
+        m = int(p * n)
+
+        for model_name, model_func in self.models.items():
+            if model_name == 'ER':
+                model_graph = model_func(n, p)
+            elif model_name == 'WS':
+                model_graph = model_func(n, k, p)
+            elif model_name == 'BA':
+                model_graph = model_func(n, m)
+            elif model_name == 'LG':
+                model_graph = self.logit_graph
+            else:
+                continue
+
+            score = self.evaluate_model(model_graph, model_name)
+            self.model_scores[model_name] = score
+
+        self.best_model = min(self.model_scores, key=self.model_scores.get)
+        return self.best_model, self.model_scores
+
